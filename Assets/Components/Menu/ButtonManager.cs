d0e87aca839b754221d557ef60c4cdbc6c74bfbe
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-08-23 09:02:35 +0200

    feat: Add pause functionality to ButtonManager
    The code changes include adding the `ShowPause` and `HidePause` methods to the `ButtonManager` class. These methods control the visibility of the pause menu and adjust the time scale accordingly. This allows the player to pause and resume the game as needed.

--- Assets/Components/Menu/ButtonManager.cs
+++ Assets/Components/Menu/ButtonManager.cs
@@ -5,17 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public void ShowPause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void HidePause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
     public void MenuScene()
     {
         SceneManager.LoadScene("Menu");
+        Time.timeScale = 1.0f;
     }
     public void CreditsScene()
     {
         SceneManager.LoadScene("Credits");
+        Time.timeScale = 1.0f;
     }
     public void GameScene()
     {
         SceneManager.LoadScene("Game");
+        Time.timeScale = 1.0f;
     }
     public void QuitGame()
     {
